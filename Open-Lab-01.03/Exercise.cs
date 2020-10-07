5ee73976dcfc051d83a909bf76fcb3aa388decee
Sebax <bugansebastian@gmail.com>
2020-10-07 10:05:30 +0200

    vypracovanie ulohy

--- Open-Lab-01.03/Exercise.cs
+++ Open-Lab-01.03/Exercise.cs
@@ -6,7 +6,8 @@ namespace Open_Lab_01._03
     {
         public int Addition(int number)
         {
-            throw new NotImplementedException();
+            number = number + 1;
+            return number;
         }
     }
 }
